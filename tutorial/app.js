6bd69f04c1f95cb49a2442a656a7f8200bfcd0ed
lucaspires-source <pires.lucas94@gmail.com>
2021-04-05 10:57:53 -0300

    Mind grenade

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,13 +1,2 @@
-//Modules
 
-//Every file in node is a module by default
-//Modules - Encapsulated Code (only share minimum)
-const names = require('./3-name.js');
-const sayHi = require('./4-utils');
-console.log(names)
-
-sayHi(names.john)
-
-sayHi(names.peter)
-
-sayHi('Susan')
+require('./6-mind-grenade.js')
